1fe15ca873e1c483dfa1b82d994a4a3a5b0bad7a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-10-14 16:44:12 -0700

    add comments

--- BitcodeEvaluation/ViewController.m
+++ BitcodeEvaluation/ViewController.m
@@ -13,10 +13,22 @@
 @interface ViewController ()
 
 @end
+/**
+ * Reads this app's main executable from disk, and replace the encrypted section with
+ * the decrypted data found in our own memory.
+ */
 static NSData* exportOwnExecutable(void (^reportError)(NSString* error)) {
+    
+    // This basically works the same way as Clutch and other application dumping
+    // tools, except, of course, this can only dump itself.
+    
+    // should we raise an error if the executable isn't encrypted?
     BOOL needsEnc = NO;
+    // Pointer to this current executable's encryption info
     struct encryption_info_command_64* enc_cmd = nil;
+    // find the first load command in this executable
     struct load_command* cmd = (struct load_command*) (((char*)&_mh_execute_header) + sizeof(struct mach_header_64));
+    // and search through all load commands in this executable.
     for (int i = 0; i < _mh_execute_header.ncmds; i++) {
         if (cmd->cmd == LC_ENCRYPTION_INFO_64) {
             enc_cmd = (struct encryption_info_command_64*)cmd;
@@ -25,10 +37,14 @@ static NSData* exportOwnExecutable(void (^reportError)(NSString* error)) {
         struct load_command* ncmd = (struct load_command*) (((char*)cmd) + cmd->cmdsize);
         cmd = ncmd;
     }
+    // If we didn't find an encryption command, executable isn't encrypted.
+    // show an error if that's enabled.
     if (needsEnc && !enc_cmd) {
+        reportError(@"Not encrypted");
         return nil;
     }
     
+    // now, load the original executable from disk
     NSBundle* bundle = [NSBundle mainBundle];
     NSMutableData* mainExecutableData = [NSMutableData dataWithContentsOfURL:bundle.executableURL];
     
@@ -37,16 +53,20 @@ static NSData* exportOwnExecutable(void (^reportError)(NSString* error)) {
         return nil;
     }
     
+    // check that the original executable is Mach-O. (I don't handle multiarch files yet)
     const uint32_t* executableData = mainExecutableData.bytes;
     if (executableData[0] != MH_MAGIC_64) {
         reportError(@"Wrong magic number");
         return nil;
     }
     
+    // If we found the encrypted section, replace the encrypted data with
+    // the decrypted copy found in our own memory.
     if (enc_cmd) {
         [mainExecutableData replaceBytesInRange:NSMakeRange(enc_cmd->cryptoff, enc_cmd->cryptsize) withBytes:((char*)&_mh_execute_header) + enc_cmd->cryptoff length:enc_cmd->cryptsize];
     }
     
+    // and finally return the data.
     return mainExecutableData;
 }
 
@@ -54,20 +74,24 @@ static NSData* exportOwnExecutable(void (^reportError)(NSString* error)) {
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (IBAction)exportPressed {
+    // runs when the Export button is pressed.
+    // stores any error messages from the dumping routine.
     __block NSString* errorString = nil;
+    // runs the dumping code.
     NSData* exportedData = exportOwnExecutable(^(NSString* err) {
         errorString = err;
     });
+    // if there was an error, display the error.
     if (errorString) {
         UIAlertController* alertController = [UIAlertController alertControllerWithTitle:@"Error during export" message:errorString preferredStyle:UIAlertControllerStyleAlert];
         [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
         [self presentViewController:alertController animated:YES completion:nil];
         return;
     }
+    // bring up a share screen of the exported data.
     UIActivityViewController* activityController = [[UIActivityViewController alloc] initWithActivityItems:@[exportedData] applicationActivities:nil];
     [self presentViewController:activityController animated:YES completion:nil];
 }
